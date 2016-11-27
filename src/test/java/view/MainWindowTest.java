6fd755acefbd27a466de17fb599d03210a97ab84
vatbub <vatbub123+github@googlemail.com>
2016-11-27 23:08:06 +0100

    Added dummyTest

---
+++ src/test/java/view/MainWindowTest.java
@@ -0,0 +1,14 @@
+package view;
+
+import org.junit.Test;
+
+/**
+ * A test for the MainWindow
+ */
+public class MainWindowTest {
+    @Test
+    public void dummyTest(){
+        System.out.println("This test does nothing but asserting true but that way, the coveralls plugin works.");
+        assert true;
+    }
+}
