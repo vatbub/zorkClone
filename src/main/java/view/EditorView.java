34f9d0e631272f066dc72ec63b4e78db47e34c93
vatbub <vatbub123+github@googlemail.com>
2016-12-22 21:23:28 +0000

    Fixed bug with the new room insertion mechanic that inserted every new room twice

--- src/main/java/view/EditorView.java
+++ src/main/java/view/EditorView.java
@@ -205,9 +205,13 @@ public class EditorView extends Application {
 
         unselectingDisabled = false;
 
-        System.out.println(event.getClickCount());
-        if (currentEditMode == EditMode.INSERT_ROOM) {
-            event.consume();
+        /*
+        event.getTarget() instanceof RoomRectangle is necessary because the event is required twice:
+            - once with event.getTarget() instanceof RoomRectangle and
+            - once with event.getTarget() instanceof Label (the name label of the room)
+        ... and we need to suppress one of the two because we don't want to insert two rooms
+         */
+        if (currentEditMode == EditMode.INSERT_ROOM && event.getTarget() instanceof RoomRectangle) {
             // add tempRoomForRoomInsertion to the game
             log.getLogger().fine("Added room to game: " + tempRoomForRoomInsertion.getRoom().getName());
             unconnectedRooms.add(tempRoomForRoomInsertion);
