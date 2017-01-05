70df1a744f78a8e000ca51adfeb4e6c62a868748
vatbub <vatbub123+github@googlemail.com>
2017-01-05 01:28:54 +0100

    Added license header to new files

--- src/main/java/model/EntityList.java
+++ src/main/java/model/EntityList.java
@@ -1,5 +1,26 @@
 package model;
 
+/*-
+ * #%L
+ * Zork Clone
+ * %%
+ * Copyright (C) 2016 - 2017 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -28,14 +49,6 @@ public class EntityList extends ArrayList<Entity> implements Serializable {
     }
 
     @Override
-    public boolean remove(Object o) {
-        for (ChangeListener changeListener : this.getChangeListenerList()) {
-            changeListener.removed((Entity) o);
-        }
-        return super.remove(o);
-    }
-
-    @Override
     public Entity set(int index, Entity element) {
         for (ChangeListener changeListener : this.getChangeListenerList()) {
             changeListener.replaced(index, this.get(index), element);
@@ -57,6 +70,14 @@ public class EntityList extends ArrayList<Entity> implements Serializable {
             changeListener.removed(this.get(index));
         }
         return super.remove(index);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (ChangeListener changeListener : this.getChangeListenerList()) {
+            changeListener.removed((Entity) o);
+        }
+        return super.remove(o);
     }
 
     public interface ChangeListener {
